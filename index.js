2565397b1796bc14c16f7666d63e1343bca9db11
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-26 00:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function fetchBooks() {
 }
 
 function renderBooks(json) {
+  console.log(json)
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
